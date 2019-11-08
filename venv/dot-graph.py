295819582208a87406f119794afd956797fcf083
wongskingwin <wongskingwin@gmail.com>
2019-11-08 11:32:07 -0500

    move header comments to GitHub README file

--- venv/dot-graph.py
+++ venv/dot-graph.py
@@ -1,11 +1,3 @@
-# Program: dot_graph.py
-#
-# Description: Plots a set of points ("base") and various transforms on them (e.g. reflection).
-#   The base points are specified as two arrays, one for the X values and the other, the Y values.
-#   The number of X and Y values must be the same.
-#
-# Created: Nov 8, 2019
-
 import matplotlib.pyplot as plt
 
 # Generate reflection points on a horizontal axis
